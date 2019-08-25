b37850bb3485665a33a7c2d4060ce780cd570d99
VitorNakama <vitorshiruo@gmail.com>
2019-08-25 04:25:23 -0300

    Updates

---
+++ precache-manifest.65f9e2ece1668cf79926824c6ba11c62.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f1addfcc57c1e40abddf890fe6c896",
+    "url": "/SiteLudoviajante/index.html"
+  },
+  {
+    "revision": "171d2787fcd4a6b7f8a4",
+    "url": "/SiteLudoviajante/static/css/main.cc2eb2c6.chunk.css"
+  },
+  {
+    "revision": "7fa34d03c6015969c2f5",
+    "url": "/SiteLudoviajante/static/js/2.c3c60be5.chunk.js"
+  },
+  {
+    "revision": "171d2787fcd4a6b7f8a4",
+    "url": "/SiteLudoviajante/static/js/main.0bff2c2c.chunk.js"
+  },
+  {
+    "revision": "ebf1fff9cce510028eee",
+    "url": "/SiteLudoviajante/static/js/runtime~main.ce168a87.js"
+  },
+  {
+    "revision": "77fa5e4df42bea60c065d98ce73378b2",
+    "url": "/SiteLudoviajante/static/media/Castelo.77fa5e4d.png"
+  },
+  {
+    "revision": "ee56e6fb8331ee7376b3495d4adaf348",
+    "url": "/SiteLudoviajante/static/media/Cocogoose Pro-trial.ee56e6fb.ttf"
+  },
+  {
+    "revision": "5554a906a67bdf03cd4cb1c73d10a460",
+    "url": "/SiteLudoviajante/static/media/Determination.5554a906.png"
+  },
+  {
+    "revision": "35dccf5bf4d54b8cf5a7f1975a1b6536",
+    "url": "/SiteLudoviajante/static/media/Estrelas.35dccf5b.png"
+  },
+  {
+    "revision": "edf0ff7fac7be99f3dbc542dd1ef0c36",
+    "url": "/SiteLudoviajante/static/media/Ludoviajante.edf0ff7f.png"
+  },
+  {
+    "revision": "7000d74cc0d1b149d705df590e46ddb8",
+    "url": "/SiteLudoviajante/static/media/Video1.7000d74c.png"
+  },
+  {
+    "revision": "b27c471aad5114ae8d2315d2d75ed087",
+    "url": "/SiteLudoviajante/static/media/Video2.b27c471a.png"
+  },
+  {
+    "revision": "c6b6ccaf7d79f944d211e0051186c4ea",
+    "url": "/SiteLudoviajante/static/media/Video3.c6b6ccaf.png"
+  },
+  {
+    "revision": "ca927a6f41d9eb97f00b59ccf7509201",
+    "url": "/SiteLudoviajante/static/media/Video4.ca927a6f.png"
+  },
+  {
+    "revision": "a8e0d71fcdf9422c5afec6657baa7f0d",
+    "url": "/SiteLudoviajante/static/media/Video5.a8e0d71f.png"
+  }
+]);
